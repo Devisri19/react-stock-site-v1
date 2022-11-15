696aadff4ab8b5a2f34228da3a104b0ebbf3278f
Devisri19 <117136153+Devisri19@users.noreply.github.com>
2022-11-15 21:57:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import GlobalStyle from './GlobalStyles';
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+      <Navbar />
+      <Hero />
+      
+     
+    </>
+  );
+}
+
+export default App;
